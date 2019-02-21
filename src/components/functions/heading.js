31ec2733be0d65b7a81c50ea9920dec562112473
yvesshum <yvesshum1210@gmail.com>
2019-02-20 18:33:21 -0600

    heading complete

--- src/components/functions/heading.js
+++ src/components/functions/heading.js
@@ -1,21 +1,43 @@
+/*
+USAGE: <Heading heading = {"Heading text here"} subheading = {"subtext here"} urgency = {integer here}/>
+
+Note: If heading string is too long, it will continue onto the next line
+Note: There is an upper margin of 33 (keep this in mind when fixing this component
+
+ */
+
 import React from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
 export default class Heading extends React.Component{
-    constructor(props) {
+    /*constructor(props) {
         super(props)
         this.state = {
             headingText: this.props.heading,
             subheadingText: this.props.subheading,
         };
     }
+    */
+
     render() {
         return(
-            <View style = {textStyles.textBox}>
-                <Text style = {textStyles.headingStyle}>{this.state.headingText}</Text>
-                <Text style = {textStyles.subheadingStyle}>{this.state.subheadingText}</Text>
+            <View style = {chooseStyle(this.props.urgency)}>
+                <Text style = {textStyles.headingStyle}>{this.props.heading}</Text>
+                <Text style = {textStyles.subheadingStyle}>{this.props.subheading}</Text>
             </View>
         )
+    }
+}
+
+function chooseStyle(urgencyNumber) { //selects which style to render based on urgency number input
+    if (urgencyNumber == 1) {
+        return textStyles.textBox1;
+    }
+    else if (urgencyNumber == 2) {
+        return textStyles.textBox2;
+    }
+    else {
+        return textStyles.textBox3;
     }
 }
 
@@ -23,6 +45,7 @@ const textStyles = StyleSheet.create({
     headingStyle: {
         marginTop: 33,
         marginLeft: 34,
+        marginRight: 34,
         fontSize: 38,
         fontFamily: 'Arial',
         textAlign: 'left',
@@ -38,9 +61,17 @@ const textStyles = StyleSheet.create({
         marginBottom: 34,
     },
 
-    textBox: {
-        //borderBottomColor: '#FDBF2E',
-        borderBottomColor: '#ffbe2e', // this looks slightly closer to the image, but maybe something's wrong with my eyes
+    textBox1: { // Most urgent
+
+        borderBottomColor: '#FC0F1B',
         borderBottomWidth: 11,
-    }
+    },
+    textBox2: {
+        borderBottomColor: '#FDBF2E',
+        borderBottomWidth: 11,
+    },
+    textBox3: {
+        borderBottomColor: '#1EB1ED',
+        borderBottomWidth: 11,
+    },
 })
