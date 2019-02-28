4c175d9086251374586ea096676e1b1fa04a8a75
yvesshum <yvesshum1210@gmail.com>
2019-02-27 19:03:51 -0600

    unfinished detailContents

--- src/components/functions/detailContents.js
+++ src/components/functions/detailContents.js
@@ -10,6 +10,7 @@ Note: this component searches through the json file and outputs corresponding va
 import React from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Heading from '../functions/heading.js';
+import CallButton from '../functions/button.js';
 
 var jsonData = require('../../assets/data.json');
 
@@ -29,6 +30,12 @@ export default class DetailContents extends React.Component{
             <View style={styles.container}>
                 <Heading heading = {this.state.name} subheading = {msg.concat(this.state.data.urgency)} urgency = {checkUrgency(this.state.data.urgency)}/>
                 <Text style = {styles.next}>Next steps: </Text>
+                <Text style = {styles.steps}>1. Gather report info. Include patient’s name, DOB, age, sex, race/ethnicity, address, and telephone number. </Text>
+                <Text style = {styles.steps}>2. Record method of diagnosis, if available</Text>
+                <Text style = {styles.steps}>3. Submit isolate or clinical materials to IDPH Lab
+                                             W. Taylor St., Chicago, IL, 60612, 312-793-1322).</Text>
+                <CallButton callNumber = {"2021232132"} subtitle = {"If unavailable, call (312) 746-5377"}/>
+
             </View>
 
         );
@@ -57,7 +64,21 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     next: {
+        marginTop: 10.5,
+        marginBottom: 10.5,
+        marginLeft: 34,
         fontFamily: 'Arial',
+        fontSize: 17,
+        fontWeight: 'bold',
+
+    },
+
+    steps: {
+        marginLeft: 34,
+        marginBottom: 19.5,
+        fontFamily: 'Arial',
+        fontSize: 13,
+        marginRight: 34,
 
     }
 
